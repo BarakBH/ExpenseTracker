c21523198102146a701728a847a090dc4886acb0
BarakBH <barakbhamu@gmail.com>
2023-05-31 18:14:08 +0300

    last components for refresher project

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,7 +49,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         }
 
@@ -71,10 +71,11 @@ const ExpenseForm = (props) => {
             </div>
             <div className="new-expense__controls">
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
+                <input type="date" min="2018-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler} />
             </div>
         </div>
         <div className="new-expense__actions">
+            <button type="button" onClick={props.onCancel}>Cancel</button>
             <button type="submit">Add Expense</button>
         </div>
     </form>
